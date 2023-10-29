a434630295fed7ed2c57ee63c16d46f2a0a6ab2c
AGgit <ag.git1111@gmail.com>
2023-10-29 16:09:15 -0600

    Added some Handling User Input elements to print user input to screen and console

--- script.js
+++ script.js
@@ -20,11 +20,35 @@ console.log(img.textContent);
 button.addEventListener('click', function (event) {
     console.log(event.type);
 });
-
+//Click any element on the screen and see it logged to the console.
 document.body.addEventListener('click', function (event) {
     console.log(event.target.tagName);  // Outputs the name of the clicked element, e.g., "BUTTON"
 });
 
 document.body.addEventListener('click', function (event) {
     console.log(event.currentTarget === document.body);  // Outputs: true
+});
+
+// Handling User Input (there are many more than demonstrated here)
+// Log text that user typed into the input field when submit button is clicked.
+submitForm.addEventListener('submit', function (event) {
+    const submitForm = document.getElementById('submitForm');
+    const inputText = document.getElementById('inputText').value; // Notice we're collecting the value
+    // Prevent the form from submitting and reloading the page:
+    event.preventDefault();
+    // Get the value of the text input:
+    logText = document.getElementById('output').textContent;
+    logText = inputText;
+
+    console.log(logText);
+});
+
+// A less verbose way to write it. This time, printing the input text below the form on the website:
+document.getElementById('submitForm').addEventListener('submit', function (event) {
+    // Prevent the form from submitting and reloading the page
+    event.preventDefault();
+    // Get the value of the text input
+    const inputText = document.getElementById('inputText').value;
+    // Display the input value in the output element
+    document.getElementById('output').textContent = inputText;
 });
